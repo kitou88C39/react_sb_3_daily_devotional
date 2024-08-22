18e396d5cfdd214dfe53e88f299dc66044df4a46
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-22 21:02:51 +0900

    src/main/java/com/example/springboot_3_daily_devotionalにmodelパッケージとDevotinal.javaを作成する

--- src/main/java/com/example/springboot_3_daily_devotional/model/Devotinal.java
+++ src/main/java/com/example/springboot_3_daily_devotional/model/Devotinal.java
@@ -1,6 +1,8 @@
 package com.example.springboot_3_daily_devotional.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +13,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Devotinal {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
 }
