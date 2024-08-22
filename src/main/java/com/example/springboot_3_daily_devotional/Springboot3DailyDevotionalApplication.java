0fbe4df500bd96edef1f96c5e423ead44843b358
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-22 17:13:18 +0900

    application.propertiesを編集する

---
+++ src/main/java/com/example/springboot_3_daily_devotional/Springboot3DailyDevotionalApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_3_daily_devotional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3DailyDevotionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3DailyDevotionalApplication.class, args);
+	}
+
+}
