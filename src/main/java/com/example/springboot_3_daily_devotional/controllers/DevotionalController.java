a0ed2c568d378df3cd635c4312d67e5dbc5ca40a
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-23 21:23:00 +0900

    src/main/java/com/example/springboot_3_daily_devotionalにcontrollersパッケージとDevotionalController.javaを作成する

--- src/main/java/com/example/springboot_3_daily_devotional/controllers/DevotionalController.java
+++ src/main/java/com/example/springboot_3_daily_devotional/controllers/DevotionalController.java
@@ -1,8 +1,11 @@
 package com.example.springboot_3_daily_devotional.controllers;
 
+import com.example.springboot_3_daily_devotional.controllers.*;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -10,5 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class DevotionalController {
 
     private final DevotionalService devotionalService;
+
+    @GetMapping("path")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
 
 }
