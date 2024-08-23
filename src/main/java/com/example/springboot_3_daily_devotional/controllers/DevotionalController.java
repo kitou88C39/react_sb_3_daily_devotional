9ed31aa3dcbb2b7b848ee79d2456c2e10a27219f
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-23 21:00:55 +0900

    src/main/java/com/example/springboot_3_daily_devotionalにcontrollersパッケージとDevotionalController.javaを作成する

--- src/main/java/com/example/springboot_3_daily_devotional/controllers/DevotionalController.java
+++ src/main/java/com/example/springboot_3_daily_devotional/controllers/DevotionalController.java
@@ -1,5 +1,10 @@
 package com.example.springboot_3_daily_devotional.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class DevotionalController {
 
 }
