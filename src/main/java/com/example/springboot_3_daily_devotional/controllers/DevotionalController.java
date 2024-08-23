0cd2023d7edf40d89864df248152b6ef2bd4d5f7
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-23 21:45:52 +0900

    src/main/java/com/example/springboot_3_daily_devotionalにcontrollersパッケージとDevotionalController.javaを作成する

--- src/main/java/com/example/springboot_3_daily_devotional/controllers/DevotionalController.java
+++ src/main/java/com/example/springboot_3_daily_devotional/controllers/DevotionalController.java
@@ -1,7 +1,12 @@
 package com.example.springboot_3_daily_devotional.controllers;
 
 import com.example.springboot_3_daily_devotional.controllers.*;
+import com.example.springboot_3_daily_devotional.services.DevotionalService;
+
 import lombok.AllArgsConstructor;
+
+import java.util.HashMap;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +19,8 @@ public class DevotionalController {
 
     @GetMapping("evotionals")
     public ResponseEntity<?> getAllDevotinals() {
-        List<Devotional> devotionals = devotionalService();
+        List<Devotional> devotionals = devotionalService.getAllDevotions();
+        Map<String, Object> map = new HashMap<>();
     }
 
 }
