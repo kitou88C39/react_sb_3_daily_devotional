457efbf57a42bff7092784de13cfc33b37a40e11
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-23 15:04:23 +0900

    src/main/java/com/example/springboot_3_daily_devotionalにmodelパッケージとDevotinal.javaを作成する

--- src/main/java/com/example/springboot_3_daily_devotional/repositories/DevotinalRepository.java
+++ src/main/java/com/example/springboot_3_daily_devotional/repositories/DevotinalRepository.java
@@ -1,5 +1,9 @@
 package com.example.springboot_3_daily_devotional.repositories;
 
-public interface DevotinalRepository {
+import java.time.LocalDate;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DevotinalRepository extends JpaRepository<Devotinal, Long> {
+    Optinal<Devotinal> findByPublishedAt(LocalDate date);
 }
