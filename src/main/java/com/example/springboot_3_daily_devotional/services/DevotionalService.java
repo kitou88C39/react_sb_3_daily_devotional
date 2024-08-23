f805c74a651d466cdfc4579c780bb1fb988580c8
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-23 15:46:51 +0900

    src/main/java/com/example/springboot_3_daily_devotionalにservicesパッケージとDevotionalService.javaを作成する

--- src/main/java/com/example/springboot_3_daily_devotional/services/DevotionalService.java
+++ src/main/java/com/example/springboot_3_daily_devotional/services/DevotionalService.java
@@ -1,5 +1,10 @@
 package com.example.springboot_3_daily_devotional.services;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class DevotionalService {
 
 }
